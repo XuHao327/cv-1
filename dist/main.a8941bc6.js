12c132a8eba30d5bd2c774472541d81fe8a5d70e
XuHao327 <86889139+XuHao327@users.noreply.github.com>
2021-09-20 23:43:43 +0800

    Update main.a8941bc6.js

--- dist/main.a8941bc6.js
+++ dist/main.a8941bc6.js
@@ -106,20 +106,7 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 })({"epB2":[function(require,module,exports) {
 var html = document.querySelector('#html');
 var style = document.querySelector('#style');
-var string = '/* \u4F60\u597D\uFF0C\u6211\u53EB\u5C0F\u5F90\n * \u63A5\u4E0B\u6765\u6211\u6F14\u793A\u4EE5\u4E0B\u6211\u7684\u524D\u7AEF\u529F\u5E95\n * \u9996\u5148\u6211\u8981\u51C6\u5907\u4E00\u4E2Adiv*/\n#div1{\n    border: 2px solid red;\n    width: 200px;\n    height: 200px;\n}\n/* \u63A5\u4E0B\u6765\u6211\u628A div \u53D8\u6210\u4E00\u4E2A\u516B\u5366\u56FE\n * \u9996\u5148\uFF0C\u628A div \u53D8\u6210\u4E00\u4E2A\u5706\n */\n#div1{\n    border-radius:50%;\n    box-shadow: 0 0 3px rgba(0,0,0,0.5);\n    border: none;\n}\n/* \u516B\u5366\u662F\u9634\u9633\u5F62\u6210\u7684\n * \u4E00\u9ED1\u4E00\u767D\n */\n#div1{\n    background: linear-gradient(90deg, rgba(255,255,255,1) 50%, rgba(2,0,36,1) 50%, rgba(0,0,0,1) 50%);\n}\n/* \u52A0\u4E24\u4E2A\u795E\u79D8\u7684\u5C0F\u7403*/\n#div1::before{\n    width: 100px;\n    height: 100px;\n    border-radius: 50%;\n    top: 0;\n    left: 50%;\n    transform: translateX(-50%);\n    background:radial-gradient(circle, rgba(2,0,29,1) 0%, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 25%, rgba(1,0,11,1) 25%, rgba(1,0,11,1) 25%, rgba(0,0,0,1) 25%, rgba(2,0,36,1) 25%, rgba(0,0,0,1) 100%);\n}\n#div1::after{\n    width: 100px;\n    height: 100px;\n    border-radius: 50%;\n    bottom: 0;\n    left: 50%;\n    transform: translateX(-50%);\n    background-color: #FFF;\n    background: radial-gradient(circle, rgba(2,0,29,1) 0%, rgba(1,0,11,1) 0%, rgba(1,0,11,1) 25%, rgba(255,255,255,1) 25%, rgba(255,255,255,1) 25%, rgba(255,255,255,1) 25%, rgba(255,255,255,1) 100%, rgba(2,0,36,1) 100%, rgba(0,0,0,1) 100%);\n#div1{
-    animation-duration: 3s;
-    animation-name: spin;
-    animation-iteration-count :infinite;
-    animation-timing-function: linear;
-}
-@keyframes spin{
-    from{
-        transform: rotate(0deg)
-    }
-    to{
-        transform: rotate(360deg)
-    }
-}}\n ';
+var string = '/* \u4F60\u597D\uFF0C\u6211\u53EB\u5C0F\u5F90\n * \u63A5\u4E0B\u6765\u6211\u6F14\u793A\u4EE5\u4E0B\u6211\u7684\u524D\u7AEF\u529F\u5E95\n * \u9996\u5148\u6211\u8981\u51C6\u5907\u4E00\u4E2Adiv*/\n#div1{\n    border: 2px solid red;\n    width: 200px;\n    height: 200px;\n}\n/* \u63A5\u4E0B\u6765\u6211\u628A div \u53D8\u6210\u4E00\u4E2A\u516B\u5366\u56FE\n * \u9996\u5148\uFF0C\u628A div \u53D8\u6210\u4E00\u4E2A\u5706\n */\n#div1{\n    border-radius:50%;\n    box-shadow: 0 0 3px rgba(0,0,0,0.5);\n    border: none;\n}\n/* \u516B\u5366\u662F\u9634\u9633\u5F62\u6210\u7684\n * \u4E00\u9ED1\u4E00\u767D\n */\n#div1{\n    background: linear-gradient(90deg, rgba(255,255,255,1) 50%, rgba(2,0,36,1) 50%, rgba(0,0,0,1) 50%);\n}\n/* \u52A0\u4E24\u4E2A\u795E\u79D8\u7684\u5C0F\u7403*/\n#div1::before{\n    width: 100px;\n    height: 100px;\n    border-radius: 50%;\n    top: 0;\n    left: 50%;\n    transform: translateX(-50%);\n    background:radial-gradient(circle, rgba(2,0,29,1) 0%, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 25%, rgba(1,0,11,1) 25%, rgba(1,0,11,1) 25%, rgba(0,0,0,1) 25%, rgba(2,0,36,1) 25%, rgba(0,0,0,1) 100%);\n}\n#div1::after{\n    width: 100px;\n    height: 100px;\n    border-radius: 50%;\n    bottom: 0;\n    left: 50%;\n    transform: translateX(-50%);\n    background-color: #FFF;\n    background: radial-gradient(circle, rgba(2,0,29,1) 0%, rgba(1,0,11,1) 0%, rgba(1,0,11,1) 25%, rgba(255,255,255,1) 25%, rgba(255,255,255,1) 25%, rgba(255,255,255,1) 25%, rgba(255,255,255,1) 100%, rgba(2,0,36,1) 100%, rgba(0,0,0,1) 100%);\n
 var string2 = '';
 var n = 0;
 
